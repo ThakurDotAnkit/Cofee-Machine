fb9c52ce67c4d27f8dfd08b4b1aaa21969d4b2e1
AnkitDotThakur <ankitlearningo@gmail.com>
2025-01-06 16:00:33 +0530

    Complete project of coffee machine! if you are intermediate in python then please go through it.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 MENU = {
     "espresso": {
         "ingredients": {
+            "milk": 0,
             "water": 50,
             "coffee": 18,
         },
@@ -24,9 +25,90 @@ MENU = {
     }
 }
 
-
+Turn_off = True
+money = 0
 resources = {
     "water": 300,
     "milk": 200,
-    "coffee": 100,
+    "coffee": 100
 }
+
+
+def check_resources(user_input):
+    for item in resources:
+        if resources[item] < user_input[item]:
+            print(f"Sorry there is not enough {item}.")
+            return False
+        return True
+
+
+def process_coin(quarters_q, dimes_q, nickles_q, pennies_q):
+    """returninh total money"""
+    quarters = 0.25
+    dimes = 0.10
+    nickles = 0.05
+    pennies = 0.01
+    total = quarters_q * quarters + dimes_q * dimes + nickles_q * nickles + pennies_q * pennies
+    return total
+
+
+def is_transection(total):
+    price = MENU[user_input]['cost']
+    if total == price:
+        print(f"This is your cofee")
+        return True
+    elif total > price:
+        print(f"Balance ${round(total - price, 2)})")
+        return True
+    else:
+        print("Sorry that's not enough money. Money refunded.")
+        return False
+
+
+def report_gen():
+    global money
+    print(f"The summary report is as following...:")
+    print(f"Water: {resources['water']}ml")
+    if user_input != "espresso":
+        print(f"milk: {resources['milk']}ml")
+    print(f"coffee: {resources['coffee']}g")
+    print(f"Money: ${money}")
+
+
+def update_resources(cofee_name):
+    global money
+    for item in resources:
+        resources[item] = resources[item] - MENU[cofee_name]['ingredients'][item]
+    money = money + MENU[cofee_name]['cost']
+    print(f"Here is your {cofee_name} ☕️. Enjoy!")
+
+
+def bill():
+    print(f"The Bill is as following...:")
+    print(f"Water: {MENU[user_input]['ingredients']['water']}")
+    if user_input != "espresso":
+        print(f"milk: {MENU[user_input]['ingredients']['milk']}")
+    print(f"coffee: {MENU[user_input]['ingredients']['coffee']}")
+    print(f"price: ${MENU[user_input]['cost']}")
+
+
+while Turn_off:
+    user_input = input('What would you like? (espresso/latte/cappuccino):')
+    if user_input.lower() == "off":
+        print("Turning off machine")
+        Turn_off = False
+    elif user_input.lower() == "report":
+        report_gen()
+    else:
+        drink = MENU[user_input]
+        if check_resources(drink['ingredients']):
+            print("please insert coins.")
+            quarters_q = int(input("How many quarters? ..:"))
+            dimes_q = int(input("How many dimes? ..:"))
+            nickles_q = int(input("How many nickles? ..:"))
+            pennies_q = int(input("How many pennies?...:"))
+            total = process_coin(quarters_q, dimes_q, nickles_q, pennies_q)
+            if is_transection(total):
+                update_resources(user_input)
+
+    print("\n"*20)
